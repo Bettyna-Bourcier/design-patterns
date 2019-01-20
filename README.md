ef7e82690c9d1ce684ad2303de5dfade21c6e144
Bettyna Bourcier <bettyna.bourcier@gmail.com>
2019-01-20 17:22:25 -0500

    Ajout description README + test unsubscribe pour le design pattern observer

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-Observer
+Ce repository est un test d'implémentation de plusieurs design patterns.
+
+#Observer pattern
+L'objectif est de séparer ce qui est observé (subject, observable) des observateurs (observers). Plusieurs observateurs peuvent réagir aux changements de l'observé.
--- src/test/java/DesignPatterns/Observer/AppTest.java
+++ src/test/java/DesignPatterns/Observer/AppTest.java
@@ -10,7 +10,7 @@ import static org.mockito.Mockito.*;
 public class AppTest 
     extends TestCase
 {
-    public void testApp()
+    public void testObserversUpdate()
     {
     	IObserver observer = mock(ObserverImpl.class);
     	IObserver observer2 = mock(ObserverImpl.class);
@@ -23,5 +23,25 @@ public class AppTest
         
         verify(observer).update();    
         verify(observer2).update();  
+    }
+    
+    public void testObserversUnsubscribe()
+    {
+    	IObserver observer = mock(ObserverImpl.class);
+    	IObserver observer2 = mock(ObserverImpl.class);
+        IObservable observable = new ObservableImpl();
+
+        observable.subscribe(observer);
+        observable.subscribe(observer2);
+        
+        observable.setState("changed");
+        
+        observable.unsubscribe(observer);
+        observable.unsubscribe(observer2);
+        
+        observable.setState("changed");
+        
+        verify(observer, times(1)).update();    
+        verify(observer2, times(1)).update();  
     }
 }
